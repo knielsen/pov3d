a5e6477693ecf128a6c4596b73e448f0f1701dd5
Kristian Nielsen <knielsen@knielsen-hq.org>
2015-05-30 10:24:08 +0200

    Now configure all three TLC5955's.
    Also remove old TLC5940 test code.

--- main.c
+++ main.c
@@ -15,19 +15,6 @@ main(void)
   setup_led();
   setup_serial();
 
-  {
-    /* Quick hack to pulse BLANK on PB0. */
-    GPIO_InitTypeDef GPIO_InitStructure;
-
-    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_25MHz;
-    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL ;
-    GPIO_Init(GPIOB, &GPIO_InitStructure);
-    GPIO_SetBits(GPIOB, GPIO_Pin_0);
-  }
   serial_puts("\r\n\r\nPOV3D Copyright 2015 Kristian Nielsen\r\n");
   serial_puts("Setting up TLCs...\r\n");
   setup_spi();
@@ -40,9 +27,6 @@ main(void)
   led_count = 0;
   for (;;)
   {
-    GPIO_SetBits(GPIOB, GPIO_Pin_0);
-    delay(2);
-    GPIO_ResetBits(GPIOB, GPIO_Pin_0);
     delay(4096*GSCLK_PERIOD*2/3);
 
     ++led_count;
--- spi.c
+++ spi.c
@@ -15,7 +15,7 @@
 
 
 static void
-setup_tlcs()
+setup_tlc_spi_dma()
 {
   GPIO_InitTypeDef GPIO_InitStructure;
   SPI_InitTypeDef SPI_InitStructure;
@@ -184,42 +184,6 @@ setup_tlcs()
 }
 
 
-/* ToDo: This is only for TLC5940 testing. */
-static void
-setup_tcl_vprg(void)
-{
-  GPIO_InitTypeDef GPIO_InitStructure;
-
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
-
-  /* GPIOA Configuration: VPRG on PA3. */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
-}
-
-
-static void
-tlc_mode_dc(void)
-{
-  delay(5);
-  GPIO_SetBits(GPIOA, GPIO_Pin_3);
-  delay(5);
-}
-
-
-static void
-tlc_mode_gs(void)
-{
-  delay(5);
-  GPIO_ResetBits(GPIOA, GPIO_Pin_3);
-  delay(5);
-}
-
-
 static void
 tlc1_latch(void)
 {
@@ -250,108 +214,6 @@ tlc3_latch(void)
   delay(1);
   GPIO_ResetBits(GPIOC, GPIO_Pin_5);
   delay(1);
-}
-
-
-/*
-  There is a slight oddity with the TLC5940, when reading out the status
-  information. The first bit of status is put into the shift register (and thus
-  onto SOUT) only at the first SCLK transition _after_ latching grayscale data.
-  So we need to pulse SCLK one extra time (effectively shifting out one dummy
-  bit) after latching, and before reading out the status information.
-*/
-static void
-tlc1_shiftout_extra_bit(void)
-{
-  GPIO_InitTypeDef GPIO_InitStructure;
-
-  /* Temporarily switch SPI pins to direct GPIO, and manually pulse one bit. */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5|GPIO_Pin_7;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-  GPIO_ResetBits(GPIOA, GPIO_Pin_7); /* SIN low (not that it matters...) */
-  GPIO_SetBits(GPIOA, GPIO_Pin_5);   /* SCLK low */
-  delay(1);
-  GPIO_ResetBits(GPIOA, GPIO_Pin_5); /* SCLK high */
-
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
-}
-
-
-static void
-tlc2_shiftout_extra_bit(void)
-{
-  GPIO_InitTypeDef GPIO_InitStructure;
-
-  /* Temporarily switch SP2 pins to direct GPIO, and manually pulse one bit. */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13|GPIO_Pin_15;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;
-  GPIO_Init(GPIOB, &GPIO_InitStructure);
-
-  GPIO_ResetBits(GPIOB, GPIO_Pin_15); /* SIN low (not that it matters...) */
-  GPIO_SetBits(GPIOB, GPIO_Pin_13);   /* SCLK low */
-  delay(1);
-  GPIO_ResetBits(GPIOB, GPIO_Pin_13); /* SCLK high */
-
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-  GPIO_Init(GPIOB, &GPIO_InitStructure);
-}
-
-
-static void
-tlc3_shiftout_extra_bit(void)
-{
-  GPIO_InitTypeDef GPIO_InitStructure;
-
-  /* Temporarily switch SP3 pins to direct GPIO, and manually pulse one bit. */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3|GPIO_Pin_5;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_DOWN;
-  GPIO_Init(GPIOB, &GPIO_InitStructure);
-
-  GPIO_ResetBits(GPIOB, GPIO_Pin_5); /* SIN low (not that it matters...) */
-  GPIO_SetBits(GPIOB, GPIO_Pin_3);   /* SCLK low */
-  delay(1);
-  GPIO_ResetBits(GPIOB, GPIO_Pin_3); /* SCLK high */
-
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-  GPIO_Init(GPIOB, &GPIO_InitStructure);
-}
-
-
-static void __attribute__((unused))
-send_to_tlc1(uint8_t *outbuf, uint8_t *inbuf, uint32_t len)
-{
-  uint32_t i;
-
-  if (!len)
-    return;
-
-  while (!(SPI1->SR & SPI_I2S_FLAG_TXE))
-    ;
-  SPI1->DR = (uint8_t)outbuf[0];
-  for (i = 1; i < len; ++i)
-  {
-    while (!(SPI1->SR & SPI_I2S_FLAG_TXE))
-      ;
-    SPI1->DR = (uint8_t)outbuf[i];
-    while (!(SPI1->SR & SPI_I2S_FLAG_RXNE))
-      ;
-    inbuf[i-1] = (uint8_t)SPI1->DR;
-  }
-  while (!(SPI1->SR & SPI_I2S_FLAG_RXNE))
-    ;
-  inbuf[len-1] = (uint8_t)SPI1->DR;
 }
 
 
@@ -445,125 +307,6 @@ dma_to_tlc3(uint8_t *outbuf, uint8_t *inbuf, uint32_t len)
 }
 
 
-#if TLC_NUM == 1
-#define tlc_latch tlc1_latch
-#define tlc_shiftout_extra_bit tlc1_shiftout_extra_bit
-#define dma_to_tlcs dma_to_tlc1
-#elif TLC_NUM == 2
-#define tlc_latch tlc2_latch
-#define tlc_shiftout_extra_bit tlc2_shiftout_extra_bit
-#define dma_to_tlcs dma_to_tlc2
-#elif TLC_NUM == 3
-#define tlc_latch tlc3_latch
-#define tlc_shiftout_extra_bit tlc3_shiftout_extra_bit
-#define dma_to_tlcs dma_to_tlc3
-#else
-#error Invalid tlc number TLC_NUM
-#endif
-
-
-static void
-setup_tlc_dc(void)
-{
-  /*
-    ToDo: Change to use TLC5955, once we have hardware.
-    For now, this tests using a string of 6 TLC5940s.
-  */
-  uint8_t dc_buf[6*16*6/8];
-  uint8_t status_buf[192*6/8];
-  uint32_t i, j, sofar, dc, bit, idx;
-
-  bit = 0;
-  sofar = 0;
-  idx = 0;
-  for (i = 0; i < 6*16; ++i)
-  {
-    dc = 1;
-    for (j = 0; j < 6; ++j)
-    {
-      sofar <<= 1;
-      sofar |= (dc & (1 << (5-j)) ? 1 : 0);
-      ++bit;
-      if (bit == 8)
-      {
-        dc_buf[idx] = sofar;
-        sofar = 0;
-        bit = 0;
-        ++idx;
-      }
-    }
-  }
-
-  serial_puts("Sending DC data to TLCs:\r\n");
-  serial_dump_buf(dc_buf, sizeof(dc_buf));
-  tlc_mode_dc();
-  /*
-    Send a dummy byte first, will be shifted out at the other end.
-    Seems to avoid a problem where we occasionally get the very first bit
-    shifted out be a "1" instead of a "0"...
-  */
-  status_buf[0] = 0;
-  dma_to_tlcs(status_buf, status_buf, 1);
-  dma_to_tlcs(dc_buf, status_buf, sizeof(dc_buf));
-  tlc_latch();
-  serial_puts("Data back from sending DC:\r\n");
-  serial_dump_buf(status_buf, sizeof(dc_buf));
-
-  serial_puts("Now latch dummy GS data, to put status info in shift register.\r\n");
-  memset(status_buf, 0x00, sizeof(status_buf));
-  tlc_mode_gs();
-  dma_to_tlcs(status_buf, status_buf, sizeof(status_buf));
-  tlc_latch();
-  serial_puts("(Data back from GS latch:)\r\n");
-  serial_dump_buf(status_buf, sizeof(status_buf));
-  serial_puts("Now reading back status info from the TLCs...\r\n");
-  memset(status_buf, 0x00, sizeof(status_buf));
-  tlc_shiftout_extra_bit();
-  dma_to_tlcs(status_buf, status_buf, sizeof(status_buf));
-  serial_puts("TLC status buffer:\r\n");
-  serial_dump_buf(status_buf, sizeof(status_buf));
-
-  serial_puts("Check that DC data is loaded correctly...");
-  for (i = 0; i < 6; ++i)
-  {
-    if (0 != memcmp(dc_buf + i*(16*6/8), status_buf + i*(192/8) + 3, (16*6/8)))
-    {
-      serial_puts(" ouch, read DC data does not match for TLC number ");
-      println_uint32(i);
-      for (;;)
-        ;
-    }
-  }
-  serial_puts(" all ok!\r\n");
-  serial_puts("Now load some GS data ...\r\n");
-  idx = 0;
-  for (i = 0; i < 16*6/3; ++i)
-  {
-    for (j = 0; j < 3; ++j)
-    {
-      uint32_t gsval = ( (i & (1<<j)) ? 4095/(4-(i/8)) : 0);
-      if (!((i*3+j) & 1))
-      {
-        status_buf[idx] = gsval >> 4;
-        ++idx;
-        status_buf[idx] = (gsval << 4) & 0xf0;
-      }
-      else
-      {
-        status_buf[idx] |= (gsval >> 8);
-        ++idx;
-        status_buf[idx] = (gsval & 0xff);
-        ++idx;
-      }
-    }
-  }
-  dma_to_tlcs(status_buf, status_buf, sizeof(status_buf));
-  tlc_latch();
-
-  serial_puts("Dat's all .oO\r\n");
-}
-
-
 static void
 add_bits(uint8_t *buf, uint32_t len, uint32_t val, uint32_t size, uint32_t *pos)
 {
@@ -641,7 +384,7 @@ fill_tlc5955_gs_latch(uint8_t *buf, uint32_t max_gs)
   {
     for (j = 0; j < 3; ++j)
     {
-      uint32_t gsval = ( (4/*i*/ & (1<<j)) ? max_gs/(2-(i/8)) : 0);
+      uint32_t gsval = ( (i & (1<<j)) ? max_gs/(2-(i/8)) : 0);
       buf[idx++] = gsval >> 8;
       buf[idx++] = gsval & 0xff;
     }
@@ -650,7 +393,8 @@ fill_tlc5955_gs_latch(uint8_t *buf, uint32_t max_gs)
 
 
 static void
-setup_tlc5955(void)
+setup_tlc5955(void (*latch_func)(void),
+              void (*dma_func)(uint8_t *, uint8_t *, uint32_t))
 {
   uint8_t databuf[97], inbuf[97], tmpbuf[97];
 
@@ -663,15 +407,15 @@ setup_tlc5955(void)
   fill_tlc5955_control_latch(databuf, 0, 0, 4);
   serial_puts("Sending control register data to TLC5955:\r\n");
   serial_dump_buf(databuf, sizeof(databuf));
-  dma_to_tlcs(databuf, inbuf, sizeof(databuf));
-  tlc_latch();
+  (*dma_func)(databuf, inbuf, sizeof(databuf));
+  (*latch_func)();
   serial_puts("Data back from sending control data:\r\n");
   serial_dump_buf(inbuf, sizeof(inbuf));
 
   serial_puts("Now latch zero GS data, and read old contents of shift register.\r\n");
   memset(tmpbuf, 0x00, sizeof(tmpbuf));
-  dma_to_tlcs(tmpbuf, inbuf, sizeof(tmpbuf));
-  tlc_latch();
+  (*dma_func)(tmpbuf, inbuf, sizeof(tmpbuf));
+  (*latch_func)();
   /* Since we shift out 7 bits too many, we need to adjust the result read. */
   shift_buf_7_bits(inbuf, 97);
   serial_puts("(Data back from GS latch:)\r\n");
@@ -686,16 +430,16 @@ setup_tlc5955(void)
     serial_puts("Nice, read control data matches\r\n");
   serial_puts("Now write control word second time to stick...\r\n");
   serial_dump_buf(databuf, sizeof(databuf));
-  dma_to_tlcs(databuf, inbuf, sizeof(databuf));
-  tlc_latch();
+  (*dma_func)(databuf, inbuf, sizeof(databuf));
+  (*latch_func)();
   serial_puts("Data back from sending control data:\r\n");
   serial_dump_buf(inbuf, sizeof(inbuf));
   serial_puts(" all ok!\r\n");
   serial_puts("Now load some GS data ...\r\n");
   fill_tlc5955_gs_latch(tmpbuf, 4095);
   serial_dump_buf(tmpbuf, sizeof(tmpbuf));
-  dma_to_tlcs(tmpbuf, inbuf, sizeof(tmpbuf));
-  tlc_latch();
+  (*dma_func)(tmpbuf, inbuf, sizeof(tmpbuf));
+  (*latch_func)();
   serial_puts("Data back from sending GS:\r\n");
   serial_dump_buf(inbuf, sizeof(inbuf));
 
@@ -706,9 +450,10 @@ setup_tlc5955(void)
 void
 setup_spi(void)
 {
-  setup_tlcs();
-  //setup_tcl_vprg();
-  /* ToDo: Setup two remaining TLCs, and nRF. */
+  setup_tlc_spi_dma();
+  /* ToDo: Setup nRF. */
 
-  setup_tlc5955();
+  setup_tlc5955(tlc1_latch, dma_to_tlc1);
+  setup_tlc5955(tlc2_latch, dma_to_tlc2);
+  setup_tlc5955(tlc3_latch, dma_to_tlc3);
 }
