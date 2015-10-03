03850abadef9c2441f7c3dff057d372f1b7f67ab
Kristian Nielsen <knielsen@knielsen-hq.org>
2015-10-03 20:51:22 +0200

    Fireworks animation, intermediate commit
    The animation works, but still some improvements and tweaks to do - colours,
    placement, etc. And needs some code cleanup.

--- gfx.c
+++ gfx.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 #include "ledtorus.h"
 
 
@@ -310,6 +312,259 @@ an_sdcard(frame_t *f, uint32_t c, void *data __attribute__((unused)))
 }
 
 
+/* Fireworks animation. */
+
+/* ToDo: Make these more generic, and move up to top. */
+
+/* Random integer 0 <= x < N. */
+static int
+irand(int n)
+{
+  return rand() / (RAND_MAX/n+1);
+}
+
+/* Random float 0 <= x <= N. */
+static float
+drand(float n)
+{
+  return (float)rand() / ((float)RAND_MAX/n);
+}
+
+/* Random unit vector of length a, uniform distribution in angular space. */
+static void
+vrand(float a, float *x, float *y, float *z)
+{
+  /*
+    Sample a random direction uniformly.
+
+    Uses the fact that cylinder projection of the sphere is area preserving,
+    so sample uniformly the cylinder, and project onto the sphere.
+  */
+  float v = drand(2.0f*F_PI);
+  float u = drand(2.0f) - 1.0f;
+  float r = sqrtf(1.0f - u*u);
+  *x = a*r*cosf(v);
+  *y = a*r*sinf(v);
+  *z = a*u;
+}
+
+
+struct st_fireworks {
+  int num_phase1;
+  int num_phase2;
+  struct { float x[3],y[3],z[3],vx,vy,vz,s,col; int base_frame, delay;
+           float gl_base, gl_period, gl_amp; } p1[10];
+  struct { float x,y,z,vx,vy,vz,col; int base_frame, delay;
+           float fade_factor; } p2[300];
+};
+
+
+static const float tang_factor = 5.0f;
+
+static void
+ut_fireworks_shiftem(struct st_fireworks *c, int i)
+{
+  int j;
+
+  for (j = sizeof(c->p1[0].x)/sizeof(c->p1[0].x[0]) - 1; j > 0; --j)
+  {
+    c->p1[i].x[j] = c->p1[i].x[j-1];
+    c->p1[i].y[j] = c->p1[i].y[j-1];
+    c->p1[i].z[j] = c->p1[i].z[j-1];
+  }
+}
+
+
+static void
+ut_fireworks_setpix(frame_t *f, float xf, float yf, float zf, float col)
+{
+  int x = roundf(xf);
+  int y = roundf(yf*tang_factor);
+  int z = roundf(zf);
+  if (y < 0)
+    y += LEDS_TANG;
+  else if (y >= LEDS_TANG)
+    y -= LEDS_TANG;
+  if (x >= 0 && x < LEDS_X && y >= 0 && y < LEDS_TANG && z >= 0 && z < LEDS_Y)
+  {
+    int r = roundf(255.0f*col);
+    int g = roundf(255.0f*col);
+    int b = roundf(255.0f*0.7f*col);
+    setpix(f, x, (LEDS_Y-1)-z, y, r, g, b);
+  }
+}
+
+
+static uint32_t
+in_fireworks(const struct ledtorus_anim *self, void **out_data)
+{
+  struct st_fireworks *c;
+  static struct st_fireworks anim_data;  /* ToDo: shared data pool. */
+
+  //*out_data = malloc(sizeof(struct st_fireworks));
+  *out_data = &anim_data;
+  c= (struct st_fireworks *)(*out_data);
+  c->num_phase1 = 0;
+  c->num_phase2 = 0;
+  return 0;
+}
+
+
+static uint32_t
+an_fireworks(frame_t *f, uint32_t frame, void *data)
+{
+  int i,j;
+
+  struct st_fireworks *c= (struct st_fireworks *)data;
+
+  static const int max_phase1 = sizeof(c->p1)/sizeof(c->p1[0]);
+  static const int max_phase2 = sizeof(c->p2)/sizeof(c->p2[0]);
+  static const float g = 0.045f;
+  static const int new_freq = 25;
+  static const float min_height = 4.0f;
+  static const float max_height = 7.0f;
+  static const int min_start_delay = 32;
+  static const int max_start_delay = 67;
+  static const int min_end_delay = 50;
+  static const int max_end_delay = 100;
+  const float V = 0.5f;
+  static const float resist = 0.11f;
+  static const float min_fade_factor = 0.22f;
+  static const float max_fade_factor = 0.27f;
+
+  /* Start a new one occasionally. */
+  if (c->num_phase1 == 0 || (c->num_phase1 < max_phase1 && irand(new_freq) == 0))
+  {
+    i = c->num_phase1++;
+
+    c->p1[i].x[0] = (float)LEDS_X/2.0f - 1.75f + drand(3.5f);
+    c->p1[i].y[0] = drand((float)LEDS_TANG/tang_factor);
+    c->p1[i].z[0] = 0.0f;
+    for (j = 0; j < sizeof(c->p1[0].x)/sizeof(c->p1[0].x[0]) - 1; ++j)
+      ut_fireworks_shiftem(c, i);
+
+    c->p1[i].vx = drand(0.35f) - 0.175f;
+    c->p1[i].vy = drand(0.35f/tang_factor) - 0.175f/tang_factor;
+    c->p1[i].s = min_height + drand(max_height - min_height);
+    c->p1[i].vz = sqrt(2*g*c->p1[i].s);
+    c->p1[i].col = 0.5;
+    c->p1[i].base_frame = frame;
+    c->p1[i].delay = min_start_delay + irand(max_start_delay - min_start_delay);
+    c->p1[i].gl_base = frame;
+    c->p1[i].gl_period = 0;
+  }
+
+  for (i = 0; i < c->num_phase1; )
+  {
+    int d = (int)frame - c->p1[i].base_frame;
+    if (d < c->p1[i].delay)
+    {
+      /* Waiting for launch - make fuse glow effect. */
+      int gl_delta = frame - c->p1[i].gl_base;
+      if (gl_delta >= c->p1[i].gl_period)
+      {
+        c->p1[i].gl_base = frame;
+        c->p1[i].gl_period = 8 + irand(6);
+        c->p1[i].gl_amp = 0.7f + drand(0.3f);
+        gl_delta = 0;
+      }
+      float glow = c->p1[i].gl_amp*sin((float)gl_delta/c->p1[i].gl_period*F_PI);
+      c->p1[i].col = roundf(0.44f + 0.31f*glow);
+      ++i;
+    }
+    else if (c->p1[i].z[0] > c->p1[i].s)
+    {
+      /* Kaboom! */
+      /* Delete this one, and create a bunch of phase2 ones (if room). */
+      int k = 10 + irand(20);
+      while (k-- > 0)
+      {
+        if (c->num_phase2 >= max_phase2)
+          break;            /* No more room */
+        j = c->num_phase2++;
+
+        /* Sample a random direction uniformly. */
+        float vx;
+        float vy;
+        float vz;
+        vrand(V, &vx, &vy, &vz);
+
+        c->p2[j].x = c->p1[i].x[0];
+        c->p2[j].y = c->p1[i].y[0];
+        c->p2[j].z = c->p1[i].z[0];
+        c->p2[j].vx = c->p1[i].vx + vx;
+        c->p2[j].vy = c->p1[i].vy + vy;
+        c->p2[j].vz = c->p1[i].vz + vz;
+        c->p2[j].col = 1.0f;
+        c->p2[j].base_frame = frame;
+        c->p2[j].delay = min_end_delay + irand(max_end_delay - min_end_delay);
+        c->p2[j].fade_factor =
+          min_fade_factor + drand(max_fade_factor - min_fade_factor);
+      }
+      c->p1[i] = c->p1[--c->num_phase1];
+    }
+    else
+    {
+      ut_fireworks_shiftem(c, i);
+      c->p1[i].col = 0.75;
+      c->p1[i].x[0] += c->p1[i].vx;
+      c->p1[i].y[0] += c->p1[i].vy;
+      c->p1[i].z[0] += c->p1[i].vz;
+      c->p1[i].vz -= g;
+      ++i;
+    }
+  }
+
+  for (i = 0; i < c->num_phase2;)
+  {
+    c->p2[i].x += c->p2[i].vx;
+    c->p2[i].y += c->p2[i].vy;
+    c->p2[i].z += c->p2[i].vz;
+
+    c->p2[i].vx -= resist*c->p2[i].vx;
+    c->p2[i].vy -= resist*c->p2[i].vy;
+    c->p2[i].vz -= resist*c->p2[i].vz + g;
+
+    float col = (1.0f/15.0f)*(15 - c->p2[i].fade_factor*(frame - c->p2[i].base_frame));
+    c->p2[i].col = col < 0.0f ? 0.0f : col;
+
+    if (c->p2[i].z <= 0.0f)
+    {
+      c->p2[i].z = 0.0f;
+      if (c->p2[i].delay-- <= 0.0f)
+      {
+        /* Delete it. */
+        c->p2[i] = c->p2[--c->num_phase2];
+      }
+      else
+        ++i;
+    }
+    else
+      ++i;
+  }
+
+  cls(f);
+  /* Mark out the "ground". */
+  for (i = 0; i < LEDS_TANG; ++i)
+    for (j = 0; j < LEDS_X; ++j)
+      setpix(f, j, LEDS_Y-1, i, 0, 0, 17);
+
+  /*
+    Draw stage2 first, so we don't overwrite a new rocket with an old, dark
+    ember.
+  */
+  for (i = 0; i < c->num_phase2; ++i)
+    ut_fireworks_setpix(f, c->p2[i].x, c->p2[i].y, c->p2[i].z, c->p2[i].col);
+  for (i = 0; i < c->num_phase1; ++i)
+  {
+    for (j = 0; j < sizeof(c->p1[0].x)/sizeof(c->p1[0].x[0]); ++j)
+      ut_fireworks_setpix(f, c->p1[i].x[j], c->p1[i].y[j], c->p1[i].z[j], c->p1[i].col);
+  }
+
+  return 0;
+}
+
+
 const struct ledtorus_anim anim_table[] = {
   { "Status",
     "Couple of scroll-texts displaying status",
@@ -326,5 +581,9 @@ const struct ledtorus_anim anim_table[] = {
   { "Simplex2",
     "Second prototype Simplex Noise animation",
     0, "SIMPLEX2.P3D", in_sdcard, an_sdcard },
+
+  { "Fireworks",
+    "Fireworks animation",
+    1024, NULL, in_fireworks, an_fireworks },
 };
 const uint32_t anim_table_length = sizeof(anim_table)/sizeof(anim_table[0]);
