b6498b61d183b23ef6a62521befac796adcee130
Kristian Nielsen <knielsen@knielsen-hq.org>
2016-04-11 09:29:14 +0200

    Add LED define in preparation for LED-torus 2

--- ledtorus.h
+++ ledtorus.h
@@ -6,6 +6,7 @@
 //#define LILLE_VIDUNDER
 
 //#define SERIAL_DBG
+//#define DEBUG_SPEED_STABILITY 1
 
 #ifdef STM32F4_DISCOVERY
 #define LED_PERIPH RCC_AHB1Periph_GPIOD
@@ -23,6 +24,12 @@
 #define LED_PERIPH RCC_AHB1Periph_GPIOC
 #define LED_GPIO GPIOC
 #define LED_PIN GPIO_Pin_13
+#endif
+
+#ifdef PCB_POV3D_2
+#define LED_PERIPH RCC_AHB1Periph_GPIOE
+#define LED_GPIO GPIOE
+#define LED_PIN GPIO_Pin_2
 #endif
 
 
